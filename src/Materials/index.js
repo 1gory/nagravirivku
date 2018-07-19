347131a9d85f525be7e3eb774b5ad7de7e96f7d8
1gory <igor.pershin.me@gmail.com>
2018-07-19 22:46:36 +0300

    improve materials

--- src/Materials/index.js
+++ src/Materials/index.js
@@ -7,6 +7,8 @@ import leather from './leather.jpg';
 import metal from './metal.jpg';
 import plastic from './plastic.jpg';
 import wood from './wood.jpg';
+import cloth from './cloth.jpg';
+import cardboard from './cardboard.jpg';
 
 const MaterialsAnchor = Scroll.Element;
 
@@ -47,9 +49,10 @@ const Material = styled.div`
 `;
 
 const RowWrapper = styled.div`
-  padding: 30px;
+  padding: 10px;
   
   @media(min-width: 768px) {
+    padding: 30px;
     margin: 0 auto;
     max-width: 1170px;
   }
@@ -59,27 +62,53 @@ const Delimiter = styled.hr`
   max-width: 50%;
 `;
 
+const Description = styled.p`
+  font-size: 12px;
+  padding-bottom: 10px;
+  font-family: 'Roboto-Light', sans-serif;
+  margin: 0;
+  text-align: center;
+  
+  @media(min-width: 768px) {
+    font-size: 13px;
+  }
+`;
+
 export default () => (
   <Wrapper>
     <MaterialsAnchor name="MaterialsAnchor" />
     <Header>C чем мы работаем</Header>
     <RowWrapper>
       <Row>
-        <Col xs={6} sm={6} md={3} lg={3}>
+        <Col xs={4} sm={4} md={2} lg={2}>
           <Img src={metal} alt="Металл" />
           <Material>Металл</Material>
+          <Description>Сталь, аллюминий, медь, латунь, драгоценные металлы.</Description>
         </Col>
-        <Col xs={6} sm={6} md={3} lg={3}>
+        <Col xs={4} sm={4} md={2} lg={2}>
           <Img src={leather} alt="Кожа" />
           <Material>Кожа</Material>
+          <Description>Кожа натуральная и искуственная, нубук, кожзам, фетр, войлок</Description>
         </Col>
-        <Col xs={6} sm={6} md={3} lg={3}>
+        <Col xs={4} sm={4} md={2} lg={2}>
           <Img src={wood} alt="Дерево" />
           <Material>Дерево</Material>
+          <Description>Фанера, МДФ, ДСП и пр.</Description>
         </Col>
-        <Col xs={6} sm={6} md={3} lg={3}>
+        <Col xs={4} sm={4} md={2} lg={2}>
+          <Img src={cardboard} alt="Пластик" />
+          <Material>Картон</Material>
+          <Description>Полиграфический картон, дизайнерский картон, Упаковочный картон</Description>
+        </Col>
+        <Col xs={4} sm={4} md={2} lg={2}>
+          <Img src={cloth} alt="Пластик" />
+          <Material>Ткань</Material>
+          <Description>Натуральная и синтетическая ткань (шелк, шифон, хлопок, лен, неопрен, органза и пр.).</Description>
+        </Col>
+        <Col xs={4} sm={4} md={2} lg={2}>
           <Img src={plastic} alt="Пластик" />
           <Material>Пластик</Material>
+          <Description>Оргстекло, пэт, полистирол, двухслойный пластик</Description>
         </Col>
       </Row>
     </RowWrapper>
